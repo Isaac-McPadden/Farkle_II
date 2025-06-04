857c4a03dedb3846d056e8530f62dc5078edaf5b
Isaac McPadden <imcpadden523@gmail.com>
2025-06-04 01:21:10 -0600

    Fixed, expanded, and passed test_strategies

--- tests/unit/test_strategies.py
+++ tests/unit/test_strategies.py
@@ -9,17 +9,25 @@ from farkle.strategies import ThresholdStrategy
         # opening-roll shortcut
         (200, 6, False, True, True, True,  True),
         # score-only
-        (250, 3, True,  True, False, True,  True),
-        (350, 3, True,  True, False, True,  False),
+        (250, 3, True,  True, False, False,  True),
+        (350, 3, True,  True, False, False,  False),
+        (250, 4, True,  True, False, False,  True),
+        (350, 4, True,  True, False, False,  False),        
         # dice-only
-        (0,   4, True,  False, True, True,  True),
-        (0,   2, True,  False, True, True,  False),
+        (0,   4, True,  False, True, False,  True),
+        (0,   3, True,  False, True, False,  False),
+        (500, 4, True,  False, True, False,  True),
+        (500, 3, True,  False, True, False,  False),
         # both + require_both
-        (200, 4, True,  True, True,  True,  True),
-        (200, 2, True,  True, True,  True,  False),
+        (400, 4, True,  True, True,  True,  False),  # Enough dice but too many points
+        (200, 2, True,  True, True,  True,  False),  # Low enough points but not enough dice
+        (200, 4, True,  True, True,  True,  True),   # Low enough points and enough dice available
+        (400, 2, True,  True, True,  True,  False),  # # Too many points and not enough dice available
         # both + OR logic
-        (200, 2, True,  True, True,  False, True),
-        (350, 2, True,  True, True,  False, False),
+        (400, 4, True,  True, True,  True,  False),  # Enough dice but too many points
+        (200, 2, True,  True, True,  True,  False),  # Low enough points but not enough dice
+        (200, 4, True,  True, True,  True,  True),   # Low enough points and enough dice available
+        (400, 2, True,  True, True,  True,  False),  # # Too many points and not enough dice available
     ],
 )
 def test_decide(tscore,dleft,has500,cs,cd,rb,exp):
